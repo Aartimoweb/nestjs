9f8b165fed4e6151a1fed999b4d3e1219d6477fe
Aartimoweb <aarti.web.moweb@gmail.com>
2025-02-06 18:28:32 +0530

    migration table

--- src/database/migrations/1738844802479-create_users_table.ts
+++ src/database/migrations/1738844802479-create_users_table.ts
@@ -3,6 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class CreateUsersTable1738844802479 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        dfs
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
