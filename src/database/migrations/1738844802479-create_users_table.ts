b04da3bab2b690b9b45ae45e4d9e184f85df27b7
Aartimoweb <aarti.web.moweb@gmail.com>
2025-02-10 10:10:05 +0530

    Update 1738844802479-create_users_table.ts

--- src/database/migrations/1738844802479-create_users_table.ts
+++ src/database/migrations/1738844802479-create_users_table.ts
@@ -3,7 +3,14 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class CreateUsersTable1738844802479 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        dfs
+       console.log("Running migration: Creating usernew table");
+        await queryRunner.query(`
+            CREATE TABLE "usernew" (
+                "id" SERIAL PRIMARY KEY,
+                "name" VARCHAR(100) NOT NULL,
+                "email" VARCHAR(100) UNIQUE NOT NULL
+            )
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
